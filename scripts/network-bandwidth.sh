2afa53a1e637d63d7e0df9dd8daaf0ccd774369f
shiyanhui <shiyanhui@bytedance.com>
2025-03-14 07:38:21 +0800

    feat: update icon

--- scripts/network-bandwidth.sh
+++ scripts/network-bandwidth.sh
@@ -60,7 +60,7 @@ main() {
     local second_measure=( $(get_bandwidth $os) )
     local download_speed=$(((${second_measure[0]} - ${first_measure[0]}) / $sleep_time))
     local upload_speed=$(((${second_measure[1]} - ${first_measure[1]}) / $sleep_time))
-    $(set_tmux_option "@network-bandwidth-previous-value" " $(format_speed $download_speed)   $(format_speed $upload_speed)")
+    $(set_tmux_option "@network-bandwidth-previous-value" " $(format_speed $download_speed)   $(format_speed $upload_speed)")
   fi
 
   echo -n "$(get_tmux_option "@network-bandwidth-previous-value")"
